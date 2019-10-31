8a3e93f6f652d18b8a7d61b150fc43f7e56bb3da
Dmitrij <dmitrij.vel@live.se>
2019-10-31 19:27:04 +0100

    a basic list can now be drawn

--- main.js
+++ main.js
@@ -55,77 +55,17 @@
 
  */
 
+const toDoListItems = [
+    "Clean house",
+    "Make dinner",
+    "make a todo list",
+];
 
-const BOARD_HEIGHT = 15;
+const listElem = document.querySelector("#todo-list");
 
-let activePlayer = 1;
-
-const toDoListItems = {};
-
-const movesMadeLabel = document.querySelector("#moves-made");
-movesMadeLabel.textContent = "To Do List";
-
-const table = document.querySelector("#game-table");
-const tbody = document.createElement("tbody");
-
-const gameInfoCell = document.querySelector("table thead tr th");
-gameInfoCell.setAttribute("colspan", "3");
-
-// create table cells
-for (let y = 1; y <= BOARD_HEIGHT; y++) {
-
-    const tableRow = document.createElement("tr");
-
-    // make a button to delete this list item
-    const deleteButton = document.createElement("td");
-    deleteButton.classList.add('titleCell');
-    tableRow.appendChild(deleteButton);
-
-    // make a button to edit this item
-    const editButton = document.createElement("td");
-    editButton.classList.add('buttonCell');
-    editButton.textContent = "time";
-    tableRow.appendChild(editButton);
-
-    // make a cell describing the task
-    const deleteCell = document.createElement("td");
-    deleteCell.classList.add('deleteCell');
-    deleteCell.textContent = "X";
-    tableRow.appendChild(deleteCell);
-
-    tbody.appendChild(tableRow);
+for (let i = 0; i < toDoListItems.length; i++) {
+    const value = toDoListItems[i];
+    const newListItem = document.createElement("li");
+    newListItem.textContent = value;
+    listElem.appendChild(newListItem);
 }
-
-// make a cell to add
-const lastTableRow = document.createElement("tr");
-const addItemButtonCell = document.createElement("td");
-addItemButtonCell.classList.add('addItemButtonCell');
-addItemButtonCell.textContent = "+";
-addItemButtonCell.setAttribute("colspan", "3");
-lastTableRow.appendChild(addItemButtonCell);
-tbody.appendChild(lastTableRow);
-
-table.appendChild(tbody);
-
-
-function clickCell(event) {
-
-    const thisCell = event.currentTarget;
-
-    // update cell appearance
-    thisCell.classList.add("ownedByPlayer"+activePlayer);
-
-    const coordinates = thisCell.getAttribute("id").match(/\d+/g);
-    const clickedCell = { x: Number(coordinates[0]), y: Number(coordinates[1]), };
-
-
-    activePlayer = (activePlayer === 1) ? 2 : 1;
-
-}
-
-function mouseEnterCell(event) {
-    const thisCell = event.currentTarget;
-    if (!(thisCell.classList.contains("ownedByPlayer1") || thisCell.classList.contains("ownedByPlayer2")))
-        thisCell.classList.add("player"+activePlayer+"Mouseover");
-}
-
