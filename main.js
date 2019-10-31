d68f1260c64fb1c6bb2339656d1737bf18df21f6
Dmitrij <dmitrij.vel@live.se>
2019-10-31 18:29:34 +0100

    added assignment description

--- main.js
+++ main.js
@@ -1,6 +1,57 @@
 /*
 
+    Din uppgift
 
+    Du skall med hjälp av HTML, CSS och Javascript göra en todo-applikation.
+    I applikationen skall en användare kunna lägga till och ta bort ärenden från en “att göra”-lista.
+    Du får göra precis som du vill med styling. Applikationen har olika krav beroende på betygsnivå.
+
+
+    Krav för godkänt (G):
+
+    Applikationen skall ha ett inmatningsfält med tillhörande knapp där användaren kan fylla i en ny todo.
+
+    När användaren klickar på en “lägg till”-knapp, eller trycker på Enter-tangenten på tangentbordet skall 
+    todo:n läggas till i en lista på todos. Inmatningsfältet skall tömmas när en todo lags till i listan.
+
+    Applikationen skall även ha en lista över redan inmatade todos.
+    I listan över inmatade todos skall det också finnas en knapp för varje todo, 
+    där användaren kan ta bort en todo ur listan. 
+    När användaren klickar på “ta bort”-knappen skall todo:n tas bort ur listan.
+
+    Applikationen skall även ha ett fält som kan användas för att filtrera todos.
+    När användaren skriver i fältet skall bara de todos som innehåller textsträngen användaren skriver in synas.
+    Om fältet är tomt skall alla todos visas.
+
+
+    Krav för väl godkänt (VG):
+
+    Applikationen skall ha ett formulär där användaren kan fylla i sin todo.
+
+    Det skall innehålla:
+    - ett textfält där själva ärendet kan skrivas in
+    - ett datumfält där ett sista datum för ärendet skall väljas, fältet skall ha dagens datum som defaultvärde
+    - en selectbox där användaren kan välja vilken kategori ärendet tillhör. Du kan själv välja vilka kategorier som 
+    skall finnas (t.ex. “hushållsarbete” eller “jobb”)
+    - en “lägg till”-knapp
+    - När användaren “skickar formuläret” (klickar på knappen) så skall ärendet läggas till i en todolista.
+
+    Applikationen skall även ha en lista över redan inmatade todos.
+    I listan skall man se själva ärendetexten, slutdatumet, kategorin för varje todo.
+    Om slutdatumet är tidigare än dagens datum ska det finnas en grafisk varning som visar att slutdatumet är passerat.
+    I listan över inmatade todos skall det också finnas en knapp för varje todo, 
+    där användaren kan ta bort en todo ur listan.
+    När användaren klickar på “ta bort”-knappen skall todo:n tas bort ur listan.
+
+    Applikationen skall även ha ett fält som kan användas för att filtrera todos.
+    När användaren skriver i fältet skall bara de todos som innehåller textsträngen användaren skriver in synas.
+    Om fältet är tomt skall alla todos visas.
+
+    Applikationen skall även ha en lista med radiobuttons, en för varje kategori, och en för “alla”.
+    Om användaren klickar i en radiobutton skall bara ärenden för den kategorin visas.
+
+    Om både en radiobutton är iklickad och fritextfältet har text skall båda användas.
+    Filtrera först fram allt som är i en viss kategori, och gör sedan fritextfiltreringen baserat på det.
 
  */
 
@@ -13,8 +64,6 @@ const toDoListItems = {};
 
 const movesMadeLabel = document.querySelector("#moves-made");
 movesMadeLabel.textContent = "To Do List";
-
-
 
 const table = document.querySelector("#game-table");
 const tbody = document.createElement("tbody");
@@ -35,12 +84,12 @@ for (let y = 1; y <= BOARD_HEIGHT; y++) {
     // make a button to edit this item
     const editButton = document.createElement("td");
     editButton.classList.add('buttonCell');
-    editButton.textContent = "E";
+    editButton.textContent = "time";
     tableRow.appendChild(editButton);
 
     // make a cell describing the task
     const deleteCell = document.createElement("td");
-    deleteCell.classList.add('buttonCell');
+    deleteCell.classList.add('deleteCell');
     deleteCell.textContent = "X";
     tableRow.appendChild(deleteCell);
 
@@ -79,15 +128,4 @@ function mouseEnterCell(event) {
     if (!(thisCell.classList.contains("ownedByPlayer1") || thisCell.classList.contains("ownedByPlayer2")))
         thisCell.classList.add("player"+activePlayer+"Mouseover");
 }
-
-
-
-
-
-
-
-
-
-
-
 
