c8f21a4eee2f481855200e30aca9c094cc728079
Dmitrijv <dmitrij.vel@live.se>
2020-01-31 18:00:47 +0100

    todo list is now saved to local storage

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ![preview](/readme/todo2.jpg)
 
-A simple To Do application that lets you add and remove new tasks, filter the list of existings tasks and. Tasks that are overdue are highlighted in red.
+A simple To Do application that lets you add and remove tasks as well as filter the list of existings tasks and by category or description.
+asks that are overdue are highlighted in red.
--- main.js
+++ main.js
@@ -55,254 +55,215 @@
 
  */
 
-const toDoItems = {
-    1:{
-        "task": "clean house",
-        "deadline": "2019-12-02",
-        "category": "Misc",
+let toDoItems = JSON.parse(localStorage.getItem("todo"));
+
+if (!toDoItems || Object.keys(toDoItems).length === 0) {
+  toDoItems = {
+    1: {
+      task: "clean house",
+      deadline: "2020-12-02",
+      category: "Misc"
     },
-    2:{
-        "task": "watch a movie",
-        "deadline": "2018-11-02",
-        "category": "R&R",
+    2: {
+      task: "watch a movie",
+      deadline: "2020-11-04",
+      category: "R&R"
     },
-    3:{
-        "task": "play football",
-        "deadline": "2019-12-03",
-        "category": "Family",
+    3: {
+      task: "bake bread with family",
+      deadline: "2020-11-04",
+      category: "Family"
     },
-    4:{
-        "task": "bake a cake",
-        "deadline": "2019-12-02",
-        "category": "Misc",
+    4: {
+      task: "move to appartment",
+      deadline: "2020-11-02",
+      category: "Misc"
     },
-    5:{
-        "task": "bake bread with family",
-        "deadline": "2019-11-03",
-        "category": "Family",
-    },
-    6:{
-        "task": "move to appartment",
-        "deadline": "2020-11-02",
-        "category": "Misc",
-    },
-    7:{
-        "task": "sell house to client",
-        "deadline": "2020-11-04",
-        "category": "Work",
-    },
+    5: {
+      task: "sell car to friend",
+      deadline: "2020-01-02",
+      category: "Work"
+    }
+  };
 }
 
-let nextId = Object.keys(toDoItems).length+1;
+let nextId = Object.keys(toDoItems).length + 1;
 
-
-
-
-const TASK_CATEGORIES = [
-    "All",
-    "Family",
-    "Work",
-    "R&R",
-    "Misc",
-];
-
+const TASK_CATEGORIES = ["All", "Family", "Work", "R&R", "Misc"];
 
 // update new errand date selector min value
 function updateMinimumDate() {
+  const currentDate = new Date();
+  const date = currentDate.getDate();
+  const month = currentDate.getMonth();
+  const year = currentDate.getFullYear();
 
-    const currentDate = new Date();
-    const date = currentDate.getDate();
-    const month = currentDate.getMonth();
-    const year = currentDate.getFullYear();
+  const yyyymmdd = year + "-" + pad(month + 1) + "-" + pad(date);
 
-    const yyyymmdd = year + "-" + pad(month + 1) + "-" + pad(date);
+  const dateInput = document.querySelector("#dateSelector");
+  dateInput.setAttribute("value", yyyymmdd);
+  dateInput.setAttribute("min", yyyymmdd);
 
-    const dateInput = document.querySelector("#dateSelector");
-    dateInput.setAttribute("value", yyyymmdd);
-    dateInput.setAttribute("min", yyyymmdd);
-
-    function pad(n) {
-        return n < 10 ? '0' + n : n;
-    }
-
-};
+  function pad(n) {
+    return n < 10 ? "0" + n : n;
+  }
+}
 updateMinimumDate();
 
 function buildRadioButtons(categories) {
-
-    const container = document.querySelector("#radioButtonContainer");
-    categories.forEach(function (category) {
-
-        const input = document.createElement("input");
-        input.setAttribute("type", "radio");
-        input.setAttribute("name", "categoryInputGroup");
-        input.setAttribute("value", category);
-        input.addEventListener("click", function(){
-            drawFilteredList(toDoItems);
-        });
-
-        if (category === "All")
-            input.checked = true;
-
-        const label = document.createElement("label");
-        label.textContent = category;
-        
-        container.appendChild(input);
-        container.appendChild(label);
-
+  const container = document.querySelector("#radioButtonContainer");
+  categories.forEach(function(category) {
+    const input = document.createElement("input");
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", "categoryInputGroup");
+    input.setAttribute("value", category);
+    input.addEventListener("click", function() {
+      drawFilteredList(toDoItems);
     });
+
+    if (category === "All") input.checked = true;
+
+    const label = document.createElement("label");
+    label.textContent = category;
+
+    container.appendChild(input);
+    container.appendChild(label);
+  });
 }
 
 buildRadioButtons(TASK_CATEGORIES);
 
-
 function fillCategoryDropDown() {
-    const select = document.querySelector('#categorySelector');
-    TASK_CATEGORIES.forEach(function (category) {
-        if (category === "All") return;
-        const option = document.createElement("option");
-        option.setAttribute("value", category);
-        option.textContent = category;
-        select.appendChild(option);
-    });
+  const select = document.querySelector("#categorySelector");
+  TASK_CATEGORIES.forEach(function(category) {
+    if (category === "All") return;
+    const option = document.createElement("option");
+    option.setAttribute("value", category);
+    option.textContent = category;
+    select.appendChild(option);
+  });
 }
 fillCategoryDropDown();
-
 
 const submitButton = document.querySelector("#newTaskSubmit");
 submitButton.addEventListener("click", createNewTask);
 
 function createNewTask() {
-
-    // task description can't be empty
-    const textInput = document.querySelector("#newTaskInput");
-    if (textInput.value.length === 0) {
-        textInput.focus();
-        textInput.classList.replace("borderWhite", "borderRed");
-        return;
-    }
-
-    // deadline can't be before current time
-    const dateInput = document.querySelector("#dateSelector");
-    if (dateInput.validity.valid === false) {
-        dateInput.focus();
-        return;
-    }
-
-    //textInput.classList.replace("borderRed", "borderWhite");
-
-    const newTask = {};
-    newTask["task"] = textInput.value;
-    newTask["category"] = document.querySelector("#categorySelector").value;
-    newTask["deadline"] = document.querySelector("#dateSelector").value
-
-    textInput.value = "";
-    dateInput.classList.remove("borderRed");
-
-    console.log(Object.keys(toDoItems).length, toDoItems);
-
-    toDoItems[nextId] = newTask;
-    nextId++;
-
+  // task description can't be empty
+  const textInput = document.querySelector("#newTaskInput");
+  if (textInput.value.length === 0) {
     textInput.focus();
-    drawFilteredList(toDoItems);
+    textInput.classList.replace("borderWhite", "borderRed");
+    return;
+  }
 
+  // deadline can't be before current time
+  const dateInput = document.querySelector("#dateSelector");
+  if (dateInput.validity.valid === false) {
+    dateInput.focus();
+    return;
+  }
+
+  const newTask = {};
+  newTask["task"] = textInput.value;
+  newTask["category"] = document.querySelector("#categorySelector").value;
+  newTask["deadline"] = document.querySelector("#dateSelector").value;
+
+  textInput.value = "";
+  dateInput.classList.remove("borderRed");
+
+  console.log(Object.keys(toDoItems).length, toDoItems);
+
+  toDoItems[nextId] = newTask;
+  nextId++;
+
+  textInput.focus();
+  localStorage.setItem("todo", JSON.stringify(toDoItems));
+
+  drawFilteredList(toDoItems);
 }
 
-
-
-const filterField = document.querySelector('#filterInput');
-filterField.addEventListener('input', function (){
-    drawFilteredList(toDoItems);
+const filterField = document.querySelector("#filterInput");
+filterField.addEventListener("input", function() {
+  drawFilteredList(toDoItems);
 });
 
 function drawFilteredList(toDoItems) {
+  const filterField = document.querySelector("#filterInput");
+  const selectedCategory = document.querySelector('input[name="categoryInputGroup"]:checked').value;
 
-    const filterField = document.querySelector('#filterInput');
-    const selectedCategory = document.querySelector('input[name="categoryInputGroup"]:checked').value;
-    
-    const filteredItems = [];
+  const filteredItems = [];
 
-    for (const index in toDoItems) {
-        const item = toDoItems[index];
-        const isCorrectCategory = (selectedCategory === 'All') || (selectedCategory === item['category'])
-        const includesKeyword = item["task"].toLowerCase().includes(filterField.value.toLowerCase());
-        if (isCorrectCategory && includesKeyword) filteredItems[index] = item;
-    }
+  for (const index in toDoItems) {
+    const item = toDoItems[index];
+    const isCorrectCategory = selectedCategory === "All" || selectedCategory === item["category"];
+    const includesKeyword = item["task"].toLowerCase().includes(filterField.value.toLowerCase());
+    if (isCorrectCategory && includesKeyword) filteredItems[index] = item;
+  }
 
-    drawTodoList(filteredItems);
-
+  drawTodoList(filteredItems);
 }
 
-
 function drawTodoList(toDoItems) {
+  const newList = document.createElement("tbody");
+  newList.setAttribute("id", "todo-table-body");
 
-    const newList = document.createElement("tbody");
-    newList.setAttribute("id", "todo-table-body");
+  for (const index in toDoItems) {
+    const item = toDoItems[index];
 
-    for (const index in toDoItems) {
+    const tr = document.createElement("tr");
 
-        const item = toDoItems[index];
+    const tdTask = document.createElement("td");
+    tdTask.textContent = `${item["task"]}`; // id[${index}]
+    tr.appendChild(tdTask);
 
-        const tr = document.createElement("tr");
+    const tdDate = document.createElement("td");
+    tdDate.textContent = item["deadline"];
+    tdDate.classList.add("dealineCell");
+    if (isOverdue(item["deadline"])) tdDate.classList.add("overdue");
+    tr.appendChild(tdDate);
 
-        const tdTask = document.createElement("td");
-        tdTask.textContent = `${item['task']}`; // id[${index}] 
-        tr.appendChild(tdTask);
+    const tdCategory = document.createElement("td");
+    tdCategory.classList.add("categoryCell");
+    tdCategory.textContent = item["category"];
+    tr.appendChild(tdCategory);
 
-        const tdDate = document.createElement("td");
-        tdDate.textContent = item['deadline'];
-        tdDate.classList.add("dealineCell");
-        if (isOverdue(item["deadline"])) tdDate.classList.add("overdue");
-        tr.appendChild(tdDate);
+    const tdDelete = document.createElement("td");
+    tdDelete.classList.add("deleteButtonCell");
+    tdDelete.setAttribute("id", "deleteItem" + index + "Button");
+    tdDelete.addEventListener("click", clickDeleteButton);
+    tdDelete.textContent = "🗑️";
+    tr.appendChild(tdDelete);
 
-        const tdCategory = document.createElement("td");
-        tdCategory.classList.add("categoryCell");
-        tdCategory.textContent = item['category'];
-        tr.appendChild(tdCategory);
+    newList.appendChild(tr);
 
-        const tdDelete = document.createElement("td");
-        tdDelete.classList.add("deleteButtonCell");
-        tdDelete.setAttribute("id", "deleteItem" + index + "Button");
-        tdDelete.addEventListener("click", clickDeleteButton);
-        tdDelete.textContent = '🗑️';
-        tr.appendChild(tdDelete);
-
-        newList.appendChild(tr);
-
-        function isOverdue(dateStr) {
-            let currentDate = new Date();
-            currentDate.setHours(0);
-            currentDate.setMinutes(0);
-            currentDate.setSeconds(0);
-            currentDate.setMilliseconds(0);
-            const targetDate = new Date(dateStr.replace(/(\d{4})-(\d{2})-(\d{2})/, "$1/$2/$3"));
-            return (targetDate - currentDate) < 0;
-        }
-
+    function isOverdue(dateStr) {
+      let currentDate = new Date();
+      currentDate.setHours(0);
+      currentDate.setMinutes(0);
+      currentDate.setSeconds(0);
+      currentDate.setMilliseconds(0);
+      const targetDate = new Date(dateStr.replace(/(\d{4})-(\d{2})-(\d{2})/, "$1/$2/$3"));
+      return targetDate - currentDate < 0;
     }
+  }
 
-    const oldList = document.querySelector("#todo-table-body");
-    document.querySelector("#todo-table").replaceChild(newList, oldList);
-
+  const oldList = document.querySelector("#todo-table-body");
+  document.querySelector("#todo-table").replaceChild(newList, oldList);
 }
 
 drawTodoList(toDoItems);
 
-
-
 function clickDeleteButton(e) {
-    const itemIndex = Number(e.currentTarget.getAttribute("id").match(/\d+/));
-    delete toDoItems[itemIndex];
-    drawFilteredList(toDoItems);
+  const itemIndex = Number(e.currentTarget.getAttribute("id").match(/\d+/));
+  delete toDoItems[itemIndex];
+  localStorage.setItem("todo", JSON.stringify(toDoItems));
+  drawFilteredList(toDoItems);
 }
 
 const taskDescriptionField = document.querySelector("#newTaskInput");
-taskDescriptionField.addEventListener('input', function (e) {
-    // reset input field border color
-    e.currentTarget.classList.remove("borderRed");
-    e.currentTarget.classList.add("borderWhite");
+taskDescriptionField.addEventListener("input", function(e) {
+  // reset input field border color
+  e.currentTarget.classList.remove("borderRed");
+  e.currentTarget.classList.add("borderWhite");
 });
-
-
-
